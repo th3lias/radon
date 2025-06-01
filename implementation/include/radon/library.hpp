4c9bac8fcdaf5ebf5ad54dddbf13c070c3ae2d5a
th3lias <emindlberger@gmail.com>
2025-06-01 10:19:31 +0200

    :👷🏼

---
+++ implementation/include/radon/library.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Elias Mindlberger on 01.06.25.
+//
+
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+
+
+class library {
+
+};
+
+
+
+#endif //LIBRARY_H
---
+++ implementation/src/library.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Elias Mindlberger on 01.06.25.
+//
+
+#include <radon/library.hpp>
